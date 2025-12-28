4c54f5c59483d41e697747d2f84aeb4ad6477b4a
OvHvO <waikitpua05@gmail.com>
2025-12-29 02:35:41 +0800

    change the tech stack bar animation time from 40s to 15s

--- src/App.jsx
+++ src/App.jsx
@@ -209,7 +209,7 @@ export default function App() {
           100% { transform: translateX(-50%); }
         }
         .animate-marquee {
-          animation: marquee 40s linear infinite;
+          animation: marquee 10s linear infinite;
         }
       `}</style>
 
